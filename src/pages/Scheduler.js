5a3fe10c9215903e8514fb68b2554a54c7d73d1a
Davide Cardia <davide.cardia@spindox.it>
2023-07-05 12:26:38 +0200

    just basic schedulerpro component

--- src/pages/Scheduler.js
+++ src/pages/Scheduler.js
@@ -12,70 +12,70 @@ import { dates, events, resources } from "./mock";
 const DEFAULT_LOCALE = "en-GB";
 
 const schedConfig = {
-  readOnly: true,
-  eventTooltip: { tooltip: { disabled: true } },
-  features: {
-    taskEdit: false,
-    regionResize: true,
-    cellEdit: false,
-    tree: {
-      expandOnCellClick: true,
-    },
-  },
-  subGridConfigs: {
-    locked: { width: 200 },
-  },
-  viewPreset: "dayAndMonth",
+  // readOnly: true,
+  // eventTooltip: { tooltip: { disabled: true } },
+  // features: {
+    // taskEdit: false,
+    // regionResize: true,
+    // cellEdit: false,
+    // tree: {
+    //   expandOnCellClick: true,
+    // },
+  // },
+  // subGridConfigs: {
+  //   locked: { width: 200 },
+  // },
+  // viewPreset: "dayAndMonth",
 };
 
-export const schedulerColumnDefs = (t) => [
-  {
-    width: 250,
-    type: "tree",
-    field: "name",
-    text: t("Scheduler.name"),
-  },
-  {
-    width: 133,
-    field: "description",
-    text: t("Scheduler.description"),
-  },
-];
+// export const schedulerColumnDefs = (t) => [
+//   {
+//     width: 250,
+//     type: "tree",
+//     field: "name",
+//     text: t("Scheduler.name"),
+//   },
+//   {
+//     width: 133,
+//     field: "description",
+//     text: t("Scheduler.description"),
+//   },
+// ];
 
 const Gantt = () => {
-  const [, setSchedRef] = useState(null);
-  const [lang, setLang] = useState(DEFAULT_LOCALE);
-  const { t, i18n } = useTranslation();
+  // const [, setSchedRef] = useState(null);
+  // const [lang, setLang] = useState(DEFAULT_LOCALE);
+  // const { t, i18n } = useTranslation();
 
-  const locales = useMemo(
-    () => ({
-      "ru-RU": Ru,
-      "en-GB": En,
-    }),
-    []
-  );
+  // const locales = useMemo(
+  //   () => ({
+  //     "ru-RU": Ru,
+  //     "en-GB": En,
+  //   }),
+  //   []
+  // );
 
-  useEffect(() => {
-    LocaleManager.locale = locales[lang];
-  }, [lang, locales]);
+  // useEffect(() => {
+  //   LocaleManager.locale = locales[lang];
+  // }, [lang, locales]);
 
-  const setLanguage = (val) => {
-    i18n.changeLanguage(val);
-    setLang(val);
-  };
+  // const setLanguage = (val) => {
+  //   i18n.changeLanguage(val);
+  //   setLang(val);
+  // };
 
   return (
     <div className="scheduler-wrapper-container">
-      <h6 onClick={() => setLanguage("en-GB")}>EN</h6>
-      <h6 onClick={() => setLanguage("ru-RU")}>RU</h6>
+      {/* <h6 onClick={() => setLanguage("en-GB")}>EN</h6>
+      <h6 onClick={() => setLanguage("ru-RU")}>RU</h6> */}
       <BryntumSchedulerPro
-        ref={setSchedRef}
-        {...schedConfig}
-        columns={schedulerColumnDefs(t)}
-        events={events}
-        resources={resources}
-        startDate={dates.startDate}
-        endDate={dates.endDate}
+        // ref={setSchedRef}
+        // {...schedConfig}
+        // columns={schedulerColumnDefs(t)}
+        // events={events}
+        // resources={resources}
+        // startDate={dates.startDate}
+        // endDate={dates.endDate}
       />
     </div>
   );
