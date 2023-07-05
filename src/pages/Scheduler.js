7ff9b739d79007a4dd7e5386356290c7779c7672
Davide Cardia <davide.cardia@spindox.it>
2023-07-05 17:43:05 +0200

    gantt -> 5.3.1

--- src/pages/Scheduler.js
+++ src/pages/Scheduler.js
@@ -5,14 +5,39 @@ import { BryntumGantt } from "@bryntum/gantt-react";
 
 import "./index.css";
 
+const schedConfig = {
+  readOnly: true,
+  eventTooltip: { tooltip: { disabled: true } },
+  features: {
+    taskEdit: false,
+    regionResize: true,
+    cellEdit: false,
+    tree: {
+      expandOnCellClick: true,
+    },
+  },
+  subGridConfigs: {
+    locked: { width: 200 },
+  },
+  viewPreset: "dayAndMonth",
+};
 
 const Gantt = () => {
   const [chart, setChart] = useState();
 
   return (
     <div className="scheduler-wrapper-container">
-      <h6 onClick={() => setChart(!chart)}>switch</h6>
-      {!chart ? <BryntumSchedulerPro /> : <BryntumGantt />}
+      <h6 onClick={() => setChart(!chart)}>
+        switch to {!chart ? "Gantt" : "Scheduler"}
+      </h6>
+      {!chart ? (
+        <BryntumSchedulerPro
+          {...schedConfig}
+          columns={[{ width: 250, type: "tree", field: "name", text: "name" }]}
+        />
+      ) : (
+        <BryntumGantt />
+      )}
     </div>
   );
 };
